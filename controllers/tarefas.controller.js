5caf95663a74ca35c81ac8b7f912811cd13eaf29
João Pedro T. <jptirso2@gmail.com>
2026-05-23 18:36:06 -0300

    Deletando e alterando tarefas

--- controllers/tarefas.controller.js
+++ controllers/tarefas.controller.js
@@ -24,14 +24,54 @@ function buscarTarefasById(req, res) {
   try {
     const id = req.params.id;
     const tarefaid = tarefas.find((tarefa) => tarefa.id == id);
-    res.status(200).json(tarefaid);
+    if (tarefaid) {
+      return res.status(200).json(tarefaid);
+    }
+    res.status(404).send("Tarefa não encontrada");
   } catch (error) {
-    res.status(404).send(error.message);
+    res.status(500).send(error.message);
   }
 }
 
-module.exports = {
-    criarTarefas,
-    buscarTarefas,
-    buscarTarefasById
+function deletarTarefa(req, res) {
+  try {
+    const id = req.params.id;
+    const indice = tarefas.findIndex((tarefa) => tarefa.id == id);
+    if (indice !== -1) {
+      tarefas.splice(indice, 1);
+      return res.status(200).send("Tarefa deletada");
+    }
+    res.status(404).send("Tarefa não encontrada");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
+function alterarTarefa(req, res) {
+  try {
+    const id = req.params.id
+    const update = req.body
+    const indice = tarefas.findIndex((tarefa) => tarefa.id == id);
+    if (indice !== -1) {
+      if (update.titulo){
+        tarefas[indice]["titulo"] = update.titulo
+      }
+      if (update.descricao){
+        tarefas[indice]["descricao"] = update.descricao
+      }
+      if (update.concluida != undefined){
+        tarefas[indice]["concluida"] = update.concluida
+      }
+      return res.status(200).json(tarefas[indice])
+    }
+    res.status(404).send("Tarefa não encontrada")
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+module.exports = {
+  criarTarefas,
+  buscarTarefas,
+  buscarTarefasById,
+  deletarTarefa,
+  alterarTarefa,
+};
--- routes/tarefas.routes.js
+++ routes/tarefas.routes.js
@@ -6,14 +6,20 @@ const {
   criarTarefas,
   buscarTarefas,
   buscarTarefasById,
+  deletarTarefa,
+  alterarTarefa,
 } = require("../controllers/tarefas.controller");
 
-const validarTarefas = require("../middlewares/validarTarefas");
+const validarTarefas = require("../middlewares/tarefas.middlewares");
 
 router.post("/tarefas", validarTarefas, criarTarefas);
 
 router.get("/tarefas", buscarTarefas);
 
 router.get("/tarefas/:id", buscarTarefasById);
+
+router.delete("/tarefas/:id", deletarTarefa )
+
+router.patch("/tarefas/:id", alterarTarefa)
 
 module.exports = router;
