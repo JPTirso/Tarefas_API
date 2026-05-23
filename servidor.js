26630da8b65ec600c795a5e8f438cc8130d5cca5
João Pedro T. <jptirso2@gmail.com>
2026-05-23 11:10:17 -0300

    Buscando a tarefa por id

--- servidor.js
+++ servidor.js
@@ -1,52 +1,59 @@
 const express = require("express");
-const crypto = require("crypto")
+const crypto = require("crypto");
 // criando meu servidor
-const app = express()
+const app = express();
 
 // transformando body em json
-app.use(express.json())
+app.use(express.json());
 
-const tarefas = []
+const tarefas = [];
 
-app.post("/tarefas", (req,res) => {
-    try{
-        // Tentativa incorreta para sistemas grandes, mas apenas usando a logica
-        // const tarefa = req.body
-        // if(tarefa.titulo){
-        //     do{  
-        //         const idExiste = false
-        //         const id = Math.random()
-        //         tarefa.map((tarefamap) => {
-        //             if(tarefamap.id == id){
-        //                 idExiste = true
-        //             }
-        //         })
-        //     }while(idExiste)
-        //     terefa.id = id
-        //     tarefas.push(tarefa)
-        // }
-        const tarefa = req.body
-        if(tarefa.titulo){
-            tarefa.id = crypto.randomUUID()
-            tarefa.concluida = false
-            tarefas.push(tarefa)
-            return res.status(201).send(`Tarefa ${tarefa.titulo} criada`)
-        }
-        res.status(400).send("Titulo não informado")
+app.post("/tarefas", (req, res) => {
+  try {
+    // Tentativa incorreta para sistemas grandes, mas apenas usando a logica
+    // const tarefa = req.body
+    // if(tarefa.titulo){
+    //     do{
+    //         const idExiste = false
+    //         const id = Math.random()
+    //         tarefa.map((tarefamap) => {
+    //             if(tarefamap.id == id){
+    //                 idExiste = true
+    //             }
+    //         })
+    //     }while(idExiste)
+    //     terefa.id = id
+    //     tarefas.push(tarefa)
+    // }
+    const tarefa = req.body;
+    if (tarefa.titulo && tarefa.titulo.trim() != "") {
+      tarefa.id = crypto.randomUUID();
+      tarefa.concluida = false;
+      tarefas.push(tarefa);
+      return res.status(201).send(`Tarefa ${tarefa.titulo} criada`);
     }
-    catch(error){
-        res.status(500).send(error.message)
-    }
-})
-app.get("/tarefas", (req,res)=>{
-    try{
-        res.status(200).json(tarefas)
-    }
-    catch(error){
-        res.status(500).send(error.message)
-    }
-})
+    res.status(400).send("Titulo não informado");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+app.get("/tarefas", (req, res) => {
+  try {
+    res.status(200).json(tarefas);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+app.get("/tarefas/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const tarefaid = tarefas.find((tarefa) => tarefa.id == id);
+    res.status(200).json(tarefaid);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 
 // botando o servidor pra rodar
 const port = 3939;
-app.listen(port, () => console.log(`Rodando na porta ${port}`))
+app.listen(port, () => console.log(`Rodando na porta ${port}`));
