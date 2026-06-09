b74be50bf47d7fe69934450bc6ab092161955cfd
JPTirso <jptirso2@gmail.com>
2026-06-09 20:38:37 -0300

    Arrumando a update das tarefas.controller

--- src/controllers/tarefas.controller.js
+++ src/controllers/tarefas.controller.js
@@ -65,18 +65,31 @@ class TarefaController {
   async update(req, res) {
     try {
       const id = req.params.id;
-      const {titulo, descricao} = req.body
-      if (!titulo || titulo.trim() == "") return res.status(400).json({message: "Titulo não informado"})
-      if (!descricao || descricao.trim() == "") return res.status(400).json({message: "Descrição não informado"})
-      const update = {titulo, descricao};
+      const userId = req.userId
+      const {titulo, descricao, concluida} = req.body
+      const update = {}
+      const tarefaAntiga = await Tarefa.findOne({
+        _id: id,
+        userId: userId
+      })
+      if(!tarefaAntiga) return res.status(404).json({message : "Tarefa não encontrada"});
+      if (titulo && titulo.trim() != "" && tarefaAntiga.titulo != titulo){
+        update.titulo = titulo
+      }
+      if (descricao && descricao.trim() != "" && tarefaAntiga.descricao != descricao){
+        update.descricao = descricao
+      }
+      if (concluida && concluida.trim() != ""  && tarefaAntiga.concluida != concluida){
+        update.concluida = concluida
+      }
+      if (Object.keys(update).length === 0) return res.status(400).json({message: "Insira alguma mudança"})
+      const __v = 1 + tarefaAntiga.__v
+      update.__v = __v
       const tarefaUpdate = await Tarefa.findByIdAndUpdate(id, update, {
         returnDocument: "after",
         runValidators: true,
       });
-      if (tarefaUpdate) {
-        return res.status(200).json(tarefaUpdate);
-      }
-      res.status(404).json({message : "Tarefa não encontrada"});
+      res.status(200).json(tarefaUpdate)
     } catch (error) {
       console.log(error)
       res.status(500).json({message: "Erro interno"});
